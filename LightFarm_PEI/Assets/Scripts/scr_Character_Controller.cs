4f771ee33f9bd9599937cb4d4600c81539373536
Scott Duff <duff.scott.richard@gmail.com>
2020-06-29 10:19:25 -0400

    First Iteration of Character Controller
    Character controller works by left clicking on a gameobject with a "ground" tag, if there is a path there the player will simply just walk there using navmesh.
    
    TODO:  implement mobile/touchscreen controls.

---
+++ LightFarm_PEI/Assets/Scripts/scr_Character_Controller.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class scr_Character_Controller : MonoBehaviour
+{
+    public LayerMask clickableArea;
+
+    private NavMeshAgent myAgent;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Moving();     
+    }
+
+   
+
+    void Moving()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(myRay, out hitInfo, clickableArea))
+            {
+                myAgent.SetDestination(hitInfo.point);
+
+            }
+        }
+
+    }
+}

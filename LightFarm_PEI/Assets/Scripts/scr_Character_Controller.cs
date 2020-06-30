b628b4e2d40f9892e01fbbfe9013e27c50f0e49a
Scott Duff <duff.scott.richard@gmail.com>
2020-06-30 14:58:57 -0400

    Updated Character Controller to handle slopes and other various things better.

--- LightFarm_PEI/Assets/Scripts/scr_Character_Controller.cs
+++ LightFarm_PEI/Assets/Scripts/scr_Character_Controller.cs
@@ -7,41 +7,87 @@ public class scr_Character_Controller : MonoBehaviour
 {
     public LayerMask clickableGroundArea;
     public LayerMask clickableFarmingPlotArea;
-    
 
+    public float rotationSpeed;
+    public float playerSpeed;
 
+    private Vector3 targetPosition;
+    private Vector3 lookAtTargetPosition;
+    private Quaternion playerRotation;
 
-    private NavMeshAgent myAgent;
+    private bool isMoving;
+    //private NavMeshAgent myAgent;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        myAgent = GetComponent<NavMeshAgent>();
+        //myAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Moving();     
+        if (Input.GetMouseButtonDown(0))
+        {
+            targetPositionToMoveTo();
+        }
+        if(isMoving == true)
+        {
+            Moving();
+        }
     }
 
    
 
-    void Moving()
+    void targetPositionToMoveTo()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hitInfo;
+       Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+       RaycastHit hitInfo;
 
-            if (Physics.Raycast(myRay, out hitInfo, clickableGroundArea))
-            {
-                myAgent.SetDestination(hitInfo.point);
+       if(Physics.Raycast(ray, out hitInfo, 100))
+         {
+            Debug.Log(hitInfo);
 
-            }
+          targetPosition = hitInfo.point;
+          targetPosition.y += 0.7f;
+          //transform.LookAt(targetPosition);
+
+          lookAtTargetPosition = new Vector3(targetPosition.x - transform.position.x, targetPosition.y - transform.position.y, targetPosition.z - transform.position.z);
+          playerRotation = Quaternion.LookRotation(lookAtTargetPosition);
+          isMoving = true;
+
         }
-
+       ///doesn't work yet.
+        //else if (Physics.Raycast(ray, out hitInfo, 100, clickableFarmingPlotArea))
+        //{
+        //    isMoving = false;
+        //}
     }
+
+    private void Moving()
+    {
+      transform.rotation = Quaternion.Slerp(transform.rotation, playerRotation, rotationSpeed * Time.deltaTime);
+      transform.position = Vector3.MoveTowards(transform.position, targetPosition, playerSpeed * Time.deltaTime);
+
+      if (transform.position == targetPosition)
+      {
+         isMoving = false;
+      }
+        
+    }
+
+
+    //if (Input.GetMouseButtonDown(0))
+    //{
+    //    Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+    //    RaycastHit hitInfo;
+
+    //    if (Physics.Raycast(myRay, out hitInfo, clickableGroundArea))
+    //    {
+    //        myAgent.SetDestination(hitInfo.point);
+
+    //    }
+    //}
 }

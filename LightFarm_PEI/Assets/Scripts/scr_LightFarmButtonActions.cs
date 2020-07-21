19dee89b60304f3586bc9887aba6d2c7e4e5ca34
Scott Duff <duff.scott.richard@gmail.com>
2020-07-21 18:31:55 -0400

    Added 2 new scripts for button actions

---
+++ LightFarm_PEI/Assets/Scripts/scr_LightFarmButtonActions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_LightFarmButtonActions : scr_abs_LightFarmButton
+{
+    //varibles go here as ussual
+
+    protected override void OnClick()
+    {
+        // tells the bvutton what to do when clicked.
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

129daad1d8f40c5b5acafa8b4b79acf5e4306dfd
Scott Duff <duff.scott.richard@gmail.com>
2020-07-22 14:50:13 -0400

    changes made to the invetory script and the new lightfarmbuttonactions script

--- LightFarm_PEI/Assets/Scripts/scr_LightFarmButtonActions.cs
+++ LightFarm_PEI/Assets/Scripts/scr_LightFarmButtonActions.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class scr_LightFarmButtonActions : scr_abs_LightFarmButton
 {
 
-    //public scr_Inventory_Manager myInventory;
+    public scr_Inventory_Manager myInventory;
+    public enum SeedType { Potato, Pea, Cauliflower, Winterwheat, Blueberry };
+
+    public enum ToolType { Basket, Hoe, Wateringcan, Fertilizer, Minerals };
+
+    public SeedType seedState;
+    public ToolType toolState;
 
     public int SeedAmount = 0;
     public int IncrementDecermentAmount = 0;
@@ -16,7 +22,52 @@ public class scr_LightFarmButtonActions : scr_abs_LightFarmButton
     {
         // tells the button what to do when clicked.
         SeedAmount += IncrementDecermentAmount;
-        //myInventory.potatoSeeds = SeedAmount;
+
+
+        switch (seedState)
+        {
+            case SeedType.Potato:
+                //do potatostuff here
+                myInventory.potatoSeeds = SeedAmount;
+                break;
+            case SeedType.Pea:
+                //do pea stuff here.
+                myInventory.peaSeeds = SeedAmount;
+                break;
+            case SeedType.Cauliflower:
+                //do califlower stuff here.
+                myInventory.cauliflowerSeeds = SeedAmount;
+                break;
+            case SeedType.Winterwheat:
+                //do winterwheat stuff here.
+                myInventory.winterWheatSeeds = SeedAmount;
+                break;
+            case SeedType.Blueberry:
+                //do blueberry seed stuff here.
+                myInventory.blueBerryBushSeeds = SeedAmount;
+                break;
+        }
+
+        switch (toolState)
+        {
+            case ToolType.Basket:
+
+                break;
+            case ToolType.Hoe:
+
+                break;
+            case ToolType.Wateringcan:
+
+                break;
+            case ToolType.Fertilizer:
+
+                break;
+            case ToolType.Minerals:
+
+                break;
+        }
+
+
         Debug.Log(SeedAmount);
 
     }
